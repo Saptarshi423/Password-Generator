ac7541b75013054f64810ac75caf5e4db28dd512
Saptarshi423 <51373298+Saptarshi423@users.noreply.github.com>
2021-08-29 12:05:36 +0530

    commit

---
+++ app.js
@@ -0,0 +1,68 @@
+console.log('hi');
+const pwEl = document.getElementById('pw');
+const copyEl = document.getElementById('copy');
+const lenEl = document.getElementById('len');
+const upperEl = document.getElementById('upper');
+const lowerEl = document.getElementById('lower');
+const numberEl = document.getElementById('number');
+const symbolEl = document.getElementById('symbol');
+const generateEl = document.getElementById('generator');
+
+const upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerLetters = "abcdefghijklmnopqrstuvwxyz";
+const numbers = '0123456789';
+const symbols = '!@#$%^&*()_+=';
+
+function getLowercase() {
+    return upperLetters[Math.floor(Math.random() *
+        upperLetters.length)];  //to get unique value of upperLetters index everytime
+}
+
+function getUppercase() {
+    return lowerLetters[Math.floor(Math.random() *
+        lowerLetters.length)];
+}
+
+function getNumber() {
+    return numbers[Math.floor(Math.random() *
+        numbers.length)];
+}
+function getSymbol() {
+    return symbols[Math.floor(Math.random() *
+        symbols.length)];
+}
+
+
+
+function generatePassword() {
+
+    const len = lenEl.value;
+    let password = "";
+    for (let i = 0; i < len; i++) {
+        const x = generateX();
+        password += x;
+    }
+
+    pwEl.innerText = password;
+}
+
+function generateX() {
+    const xs = [];
+    if (upperEl.checked) {
+        xs.push(getUppercase());
+    }
+    if (lowerEl.checked) {
+        xs.push(getLowercase());
+    }
+    if (numberEl.checked) {
+        xs.push(getNumber());
+    }
+    if (symbolEl.checked) {
+        xs.push(getSymbol());
+    }
+    if(screenX.length==0) return "";
+    //console.log(xs);
+    return xs[Math.floor(Math.random() * xs.length)];
+}
+
+generateEl.addEventListener('click', generatePassword);
